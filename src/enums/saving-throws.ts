f8bd29dc7f8766730ddb0f3b9a5eac84cdcdd5d1
Michael Gamlem III <mgamlem3@gmail.com>
2023-02-23 13:16:05 -0800

    Add numbers to saving-throws

--- src/enums/saving-throws.ts
+++ src/enums/saving-throws.ts
@@ -5,13 +5,13 @@
  */
 
 export enum SavingThrow {
-	None,
-	Charisma,
-	Constitution,
-	Dexterity,
-	Intelligence,
-	Strength,
-	Wisdom,
+	None = 0,
+	Charisma = 1,
+	Constitution = 2,
+	Dexterity = 3,
+	Intelligence = 4,
+	Strength = 5,
+	Wisdom = 6,
 }
 
 export const mapNumberToSavingThrow = (x: number): SavingThrow | undefined => {
