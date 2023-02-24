e962f6be749d9473d7cb70e249f8c98d223e38f4
Michael Gamlem III <mgamlem3@gmail.com>
2023-02-24 12:08:12 -0800

    Use splitVendorChunkPlugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-/** @format */
+/**
+ * @author Michael Gamlem III
+ * @copyright This file is subject to the terms and conditions defined in file 'LICENSE', which is part of the source code for this project.
+ * @format
+ */
 
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), splitVendorChunkPlugin()],
 });
