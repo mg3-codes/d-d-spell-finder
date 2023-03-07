55db75cd7f5cd022fafe7cd145e1351f90e132f1
Michael Gamlem III <mgamlem3@gmail.com>
2023-03-07 12:55:10 -0800

    Add rollbar

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider as RollbarProvider, ErrorBoundary } from "@rollbar/react";
 
 import { ColumnContextProvider } from "./components/column-context-provider";
 import LoadingSpinner from "./components/loading-spinner";
@@ -15,6 +16,15 @@ import { SelectedRowContextProvider } from "./components/selected-row-context-pr
 
 const Index = React.lazy(() => import("./components/routes"));
 const Export = React.lazy(() => import("./components/routes/export"));
+
+const rollbarConfig = {
+	accessToken: "9da11dd53b1c4323a3fb09861e6dd841",
+	environment: import.meta.env.MODE,
+	version: "0.1.6",
+	enabled: import.meta.env.MODE === "production",
+	captureUncaught: true,
+	captureUnhandledRejections: true,
+};
 
 const router = createBrowserRouter([
 	{
@@ -29,14 +39,18 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<ThemeContextProvider>
-			<ColumnContextProvider>
-				<SelectedRowContextProvider>
-					<Suspense fallback={<LoadingSpinner />}>
-						<RouterProvider router={router} />
-					</Suspense>
-				</SelectedRowContextProvider>
-			</ColumnContextProvider>
-		</ThemeContextProvider>
+		<RollbarProvider config={rollbarConfig}>
+			<ErrorBoundary>
+				<Suspense fallback={<LoadingSpinner />}>
+					<ThemeContextProvider>
+						<ColumnContextProvider>
+							<SelectedRowContextProvider>
+								<RouterProvider router={router} />
+							</SelectedRowContextProvider>
+						</ColumnContextProvider>
+					</ThemeContextProvider>
+				</Suspense>
+			</ErrorBoundary>
+		</RollbarProvider>
 	</React.StrictMode>,
 );
