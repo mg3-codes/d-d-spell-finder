b6df776d4211a969e9e53e0b875939ab378367e5
Michael Gamlem III <mgamlem3@gmail.com>
2023-04-25 19:40:04 -0700

    Don't use string concatenation

--- src/utility/cookies.ts
+++ src/utility/cookies.ts
@@ -15,8 +15,8 @@ export const setCookie = (
 };
 
 export const getCookie = (name: string): string | null => {
-	const value = "; " + document.cookie;
-	const parts = value.split("; " + name + "=");
+	const value = `; ${document.cookie}`;
+	const parts = value.split(`; ${name}=`);
 
 	if (parts.length === 2) return parts?.pop()?.split(";")?.shift() ?? null;
 
