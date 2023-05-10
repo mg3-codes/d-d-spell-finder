5fa9c809167b7e04b988572111330e2585f6327d
Michael Gamlem III <mgamlem3@gmail.com>
2023-05-09 19:39:19 -0700

    0.3.2

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -52,7 +52,7 @@ const Footer = () => {
 
 	return (
 		<div className="footer">
-			<span>Version: 0.3.1</span>
+			<span>Version: 0.3.2</span>
 			<div>
 				<Button variant="link" onClick={openHelpModal}>
 					Help
