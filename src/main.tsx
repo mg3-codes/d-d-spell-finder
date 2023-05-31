ec1157505516eced4cfbebac58ac83433c95297f
Michael Gamlem III <mgamlem3@gmail.com>
2023-05-30 21:05:21 -0700

    0.4.2

--- src/main.tsx
+++ src/main.tsx
@@ -30,7 +30,7 @@ const rollbarConfig = {
 	enabled: import.meta.env.MODE === "production",
 	captureUncaught: true,
 	captureUnhandledRejections: true,
-	code_version: "0.4.1",
+	code_version: "0.4.2",
 	source_map_enabled: true,
 };
 
