57cc751be32107dfa3da69f3c7c582c37757911c
mohibkay <mohibk.in@gmail.com>
2023-06-10 09:56:34 +0530

    refactor: bootstrap white color in dark mode

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -73,7 +73,7 @@ const Footer = () => {
 				</Button>
 			</div>
 			<button className="github-button" onClick={openGithub}>
-				<i className="bi bi-github github-icon text-primary" />
+				<i className="bi bi-github github-icon" />
 			</button>
 			<Modal show={modalIsOpen} onHide={closeModal}>
 				<Modal.Header closeButton>
