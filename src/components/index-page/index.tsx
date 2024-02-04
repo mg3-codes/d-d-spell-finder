34ecdd96c42a027b0b8c027cc745cf2f7e2d1844
Michael Gamlem III <mgamlem3@gmail.com>
2024-02-03 20:11:52 -0800

    Remove unused import

--- src/components/index-page/index.tsx
+++ src/components/index-page/index.tsx
@@ -14,7 +14,6 @@ const Footer = React.lazy(() => import("../footer"));
 import { SearchBar } from "../search-bar";
 
 import "./page.scss";
-import { on } from "events";
 
 export const IndexPage = () => {
 	const [searchParams] = useSearchParams();
